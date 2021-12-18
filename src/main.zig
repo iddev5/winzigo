4450eb42ffee9f05011c37e08e6254a4e5890c38
iddev5 <ayushbardhan5@gmail.com>
2021-12-18 18:31:36 +0530

    xcb+main: Added add remaining key symbol conversions

--- src/main.zig
+++ src/main.zig
@@ -38,6 +38,7 @@ pub const Key = enum(u8) {
     x,
     y,
     z,
+
     zero,
     one,
     two,
@@ -48,7 +49,91 @@ pub const Key = enum(u8) {
     seven,
     eight,
     nine,
-    ten,
+
+    f1,
+    f2,
+    f3,
+    f4,
+    f5,
+    f6,
+    f7,
+    f8,
+    f9,
+    f10,
+    f11,
+    f12,
+    f13,
+    f14,
+    f15,
+    f16,
+    f17,
+    f18,
+    f19,
+    f20,
+    f21,
+    f22,
+    f23,
+    f24,
+    f25,
+
+    kp_divide,
+    kp_multiply,
+    kp_subtract,
+    kp_add,
+    kp_0,
+    kp_1,
+    kp_2,
+    kp_3,
+    kp_4,
+    kp_5,
+    kp_6,
+    kp_7,
+    kp_8,
+    kp_9,
+    kp_decimal,
+    kp_equal,
+    kp_enter,
+
+    @"return",
+    escape,
+    tab,
+    left_shift,
+    right_shift,
+    left_control,
+    right_control,
+    left_alt,
+    right_alt,
+    left_super,
+    right_super,
+    menu,
+    num_lock,
+    caps_lock,
+    print,
+    scroll_lock,
+    pause,
+    delete,
+    home,
+    end,
+    page_up,
+    page_down,
+    insert,
+    left,
+    right,
+    up,
+    down,
+    backspace,
+    space,
+    minus,
+    equal,
+    left_bracket,
+    right_bracket,
+    backslash,
+    semicolon,
+    apostrophe,
+    comma,
+    period,
+    slash,
+    grave,
     unknown,
 };
 
@@ -84,7 +169,7 @@ pub const WindowInfo = struct {
     height: u16 = 256,
 };
 fn CoreType() type {
-    return xcb;   
+    return xcb;
 }
 
 fn WindowType() type {
@@ -93,23 +178,23 @@ fn WindowType() type {
 
 pub const Core = struct {
     internal: CoreType(),
-    
+
     pub fn init() !Core {
         return Core{ .internal = try CoreType().init() };
     }
-    
+
     pub fn deinit(core: *Core) void {
         core.internal.deinit();
     }
-    
+
     pub fn createWindow(core: *Core, info: WindowInfo) Window {
         return .{ .internal = core.internal.createWindow(info) };
     }
-    
+
     pub fn pollEvent(core: *Core) ?Event {
         return core.internal.pollEvent();
     }
-    
+
     pub fn waitEvent(core: *Core) ?Event {
         return core.internal.waitEvent();
     }
@@ -117,23 +202,23 @@ pub const Core = struct {
 
 pub const Window = struct {
     internal: WindowType(),
-  
+
     pub fn init(info: WindowInfo) Window {
         return .{ .internal = WindowType().init(info) };
     }
-    
+
     pub fn initFromInternal(internal: WindowType()) Window {
         return .{ .internal = internal };
     }
-    
+
     pub fn deinit(window: *Window) void {
         window.internal.deinit();
     }
-    
+
     pub fn setTitle(window: *Window, title: []const u8) void {
         window.internal.setTitle(title);
     }
-    
+
     pub fn setSize(window: *Window, width: u16, height: u16) void {
         window.internal.setSize(width, height);
     }
