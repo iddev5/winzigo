64a1b33f0f5e7d21293fe35c3fc8e978fa3d961e
iddev5 <ayushbardhan5@gmail.com>
2021-12-31 21:03:49 +0530

    build: Implement experimental mains support into build system

--- build.zig
+++ build.zig
@@ -4,7 +4,41 @@ const examples = .{
     "events",
     "window",
     "input",
+    "wasm",
 };
+
+pub fn createApplication(b: *std.build.Builder, name: []const u8, path: []const u8, target: std.zig.CrossTarget) *std.build.LibExeObjStep {
+    const lib = std.build.Pkg{
+        .name = "winzigo",
+        .path = .{ .path = "src/main.zig" },
+    };
+
+    if (target.toTarget().cpu.arch == .wasm32) {
+        const application = b.addSharedLibrary(name, "src/mains/wasm.zig/", .unversioned);
+        application.setTarget(target);
+        application.addPackage(.{
+            .name = "app",
+            .path = .{ .path = path },
+            .dependencies = &.{lib},
+        });
+        application.install();
+
+        return application;
+    } else {
+        const application = b.addExecutable(name, "src/mains/native.zig");
+        application.setTarget(target);
+        application.linkLibC();
+        application.linkSystemLibrary("xcb");
+        application.addPackage(.{
+            .name = "app",
+            .path = .{ .path = path },
+            .dependencies = &.{lib},
+        });
+        application.install();
+
+        return application;
+    }
+}
 
 pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
@@ -17,30 +51,22 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const lib = std.build.Pkg{
-        .name = "winzigo",
-        .path = .{ .path = "src/main.zig" },
-    };
-
     inline for (examples) |eg| {
-        const example = b.addExecutable(eg, "examples/" ++ eg ++ ".zig");
-        example.setTarget(target);
+        const example = createApplication(b, eg, "examples/" ++ eg ++ ".zig", target);
         example.setBuildMode(mode);
-        example.linkLibC();
-        example.linkSystemLibrary("xcb");
-        example.addPackage(lib);
-        example.install();
 
         const make_step = b.step("make-" ++ eg, "Build the " ++ eg ++ " example");
         make_step.dependOn(&example.install_step.?.step);
 
-        const run_cmd = example.run();
-        run_cmd.step.dependOn(&example.install_step.?.step);
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
+        if (target.toTarget().cpu.arch != .wasm32) {
+            const run_cmd = example.run();
+            run_cmd.step.dependOn(&example.install_step.?.step);
+            if (b.args) |args| {
+                run_cmd.addArgs(args);
+            }
 
-        const run_step = b.step("run-" ++ eg, "Run the " ++ eg ++ " example");
-        run_step.dependOn(&run_cmd.step);
+            const run_step = b.step("run-" ++ eg, "Run the " ++ eg ++ " example");
+            run_step.dependOn(&run_cmd.step);
+        }
     }
 }
