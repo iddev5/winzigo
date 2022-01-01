764574bed46697e4de887869107d20b6746be467
iddev5 <ayushbardhan5@gmail.com>
2022-01-01 23:55:37 +0530

    examples: port events.zig to new API

--- examples/events.zig
+++ examples/events.zig
@@ -1,57 +1,64 @@
 const std = @import("std");
 const winzigo = @import("winzigo");
 
-pub fn main() anyerror!void {
-    var core = try winzigo.init();
-    defer core.deinit();
+var core: winzigo = undefined;
+
+pub fn init() !void {
+    core = try winzigo.init();
+    errdefer core.deinit();
 
     var window = core.createWindow(.{});
-    defer window.deinit();
+    errdefer window.deinit();
 
-    var is_running: bool = true;
-    while (is_running) {
-        while (core.pollEvent()) |event| {
-            switch (event.ev) {
-                .key_press => |ev| {
-                    if (ev.key == .escape) is_running = false;
-                    std.log.info("key pressed {s}", .{@tagName(ev.key)});
-                },
-                .key_release => |ev| {
-                    std.log.info("key released {s}", .{@tagName(ev.key)});
-                },
-                .button_press => |ev| {
-                    std.log.info("button pressed {s}", .{@tagName(ev.button)});
-                },
-                .button_release => |ev| {
-                    std.log.info("button released {s}", .{@tagName(ev.button)});
-                },
-                .mouse_scroll => |ev| {
-                    std.log.info("mouse scroll x: {} y: {}", .{ ev.scroll_x, ev.scroll_y });
-                },
-                .mouse_motion => |ev| {
-                    std.log.info("mouse pos x: {} y: {}", .{ ev.x, ev.y });
-                },
-                .mouse_enter => |ev| {
-                    std.log.info("mouse entered window at x: {} y: {}", .{ ev.x, ev.y });
-                },
-                .mouse_leave => |ev| {
-                    std.log.info("mouse left window at x: {} y: {}", .{ ev.x, ev.y });
-                },
-                .focus_in => |_| {
-                    std.log.info("gained focus", .{});
-                },
-                .focus_out => |_| {
-                    std.log.info("lost focus", .{});
-                },
-                .quit => |_| {
-                    std.log.info("quit", .{});
-                    is_running = false;
-                },
-            }
+    _ = window;
+}
+
+pub fn update() !bool {
+    while (core.pollEvent()) |event| {
+        switch (event.ev) {
+            .key_press => |ev| {
+                if (ev.key == .escape) return false;
+                std.log.info("key pressed {s}", .{@tagName(ev.key)});
+            },
+            .key_release => |ev| {
+                std.log.info("key released {s}", .{@tagName(ev.key)});
+            },
+            .button_press => |ev| {
+                std.log.info("button pressed {s}", .{@tagName(ev.button)});
+            },
+            .button_release => |ev| {
+                std.log.info("button released {s}", .{@tagName(ev.button)});
+            },
+            .mouse_scroll => |ev| {
+                std.log.info("mouse scroll x: {} y: {}", .{ ev.scroll_x, ev.scroll_y });
+            },
+            .mouse_motion => |ev| {
+                std.log.info("mouse pos x: {} y: {}", .{ ev.x, ev.y });
+            },
+            .mouse_enter => |ev| {
+                std.log.info("mouse entered window at x: {} y: {}", .{ ev.x, ev.y });
+            },
+            .mouse_leave => |ev| {
+                std.log.info("mouse left window at x: {} y: {}", .{ ev.x, ev.y });
+            },
+            .focus_in => |_| {
+                std.log.info("gained focus", .{});
+            },
+            .focus_out => |_| {
+                std.log.info("lost focus", .{});
+            },
+            .quit => |_| {
+                std.log.info("quit", .{});
+                return false;
+            },
         }
-
-        std.os.nanosleep(0, 16000000);
     }
+
+    return true;
+}
+
+pub fn deinit() void {
+    core.deinit();
     std.log.info("All your queued events are belong to us.", .{});
 }
 
